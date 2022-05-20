7460c4306cf4571b0443fc21d5744fce67da0bf2
rohulaminwd <rohulaminwd@gmail.com>
2022-05-20 21:33:56 +0600

    some comment add

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+// jwt token function
 function verifyJWT(req, res, next){
   const authorization = req.headers.authorization;
   if(!authorization){
